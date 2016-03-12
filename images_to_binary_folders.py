3615954cf86552b67b29f325e24101ecfd36baf8
RobSalzwedel <rob.salzwedel@gmail.com>
2016-03-12 08:35:12 +0200

    Create a dict of photo ID's and labels

--- images_to_binary_folders.py
+++ images_to_binary_folders.py
@@ -7,4 +7,11 @@ import numpy as np
 photos_biz = pd.read_csv('~/Projects/data/yelp/train_photo_to_biz_ids.csv')
 
 # List of businesses with string indicating relevant tags
-biz_label = pd.read_csv('~/Projects/data/yelp/train_photo_to_biz_ids.csv')
+biz_label = pd.read_csv('~/Projects/data/yelp/train.csv')
+
+# Create a dictionary with photo_id as the key and labels as the value
+d = {}
+for j in photos_biz.index:
+    d[photos_biz['photo_id'][j]] = biz_label['labels'][biz_label['business_id'][biz_label['business_id'] == photos_biz['business_id'][j]].index[0]]
+        
+        
